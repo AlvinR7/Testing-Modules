e9ade17517cf99ce198543e61a538292c13a1bf1
Alvin Rao <alvinrao7@gmail.com>
2019-01-11 22:19:22 +0000

    added testingmodule

---
+++ testingmodule.js
@@ -0,0 +1,10 @@
+var exportingNumber = require("./exportmodules");
+
+console.log(exportingNumber.storeNumber(1));
+console.log(exportingNumber.storeNumber(8));
+console.log(exportingNumber.storeNumber(5));
+console.log(exportingNumber.storeNumber(4));
+
+console.log(exportingNumber.assortArray());
+
+
